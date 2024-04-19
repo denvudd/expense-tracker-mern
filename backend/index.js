4e838856d0447f42a43225619116a011029a897a
denvudd <dimontv7@gmail.com>
2024-04-19 14:44:25 +0300

    be/init: passport-js, mongodb sessions

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,31 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { buildContext } from "graphql-passport";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./type-defs/index.js";
 import { connectDB } from "./db/connectDB.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (error) => console.log(error));
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -23,6 +35,22 @@ const server = new ApolloServer({
 });
 
 await server.start();
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // this option specifies whether to save the session to the store on every request
+    saveUninitialized: false, // option specifies whether to save uninitialized sessions
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
+    },
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
@@ -36,7 +64,7 @@ app.use(
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req, res }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req }),
   })
 );
 
